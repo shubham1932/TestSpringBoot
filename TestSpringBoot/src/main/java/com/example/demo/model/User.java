c3f1cac6c50f009135efef171dee5fb7d5d71074
LAPTOP-8CV8LAPR\Shubham <shubham1932@gmail.com>
2019-07-24 20:31:25 +0530

    committed changes

--- TestSpringBoot/src/main/java/com/example/demo/model/User.java
+++ TestSpringBoot/src/main/java/com/example/demo/model/User.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import com.example.demo.enums.RoleTypeEnum;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Getter;
