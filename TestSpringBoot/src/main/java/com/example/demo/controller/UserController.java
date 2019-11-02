0870358398df140a81534b769630345615ddc15b
LAPTOP-8CV8LAPR\Shubham <shubham1932@gmail.com>
2019-11-02 15:59:05 +0530

    Committed changes from Divya for testing

--- TestSpringBoot/src/main/java/com/example/demo/controller/UserController.java
+++ TestSpringBoot/src/main/java/com/example/demo/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.model.User;
 import com.example.demo.model.UserListResponse;
 import com.example.demo.service.UserService;
